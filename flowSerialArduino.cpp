499e0a0b94289f183c43db1f368b2737b5a9eed4
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-25 20:02:20 +0100

    Initial release

---
+++ flowSerialArduino.cpp
@@ -0,0 +1,262 @@
+
+#include "Arduino.h"
+#include "flowSerialArduino.h"
+
+//FSM states
+#define idle                0x00
+#define startRecieved       0x01
+#define instructionRecieved 0x02
+#define argumentsRecieved   0x03
+#define LSBchecksumRecieved 0x04
+#define MSBchecksumRecieved 0x05
+#define executeInstruction  0x06
+
+//instruction codes
+#define IDrequest           0x00
+#define readRequest         0x01
+#define writeInstruction        0x02
+#define dataReturn          0x03
+#define debugInfo           0x04
+#define debugStateCommand   0x05
+#define transmissionErrorInstruction   0x06
+
+FlowSerial::FlowSerial(int baudrate, int regSize, char IDinit){
+	Serial.begin(baudrate);
+	ID = IDinit;
+	debugEnable = false;
+	Serial.flush();
+	inboxRegisterAt = 0;
+	inboxAvailable = 0;
+}
+
+char FlowSerial::update(){
+	while(Serial.available() != 0){
+		unsigned char byteIn = Serial.read();
+		switch(process){
+			case idle:
+				if(byteIn == 0xAA){
+					checksumInbox = 0xAA;
+					timeoutTime = millis();
+					process = startRecieved;
+					if(debugEnable == true){
+						sendDebugInfo("0xAA recieved.", 14);
+					}
+				}
+				break;
+			case startRecieved:
+				instruction = byteIn;
+				checksumInbox += byteIn;
+				argumentBufferAt = 0;
+				process = instructionRecieved;
+				if(debugEnable == true){
+					Serial.println("instruction recieved");
+				}
+				break;
+			case instructionRecieved:
+				if(instruction != IDrequest){
+					checksumInbox += byteIn;
+				}
+				switch(instruction){
+					case IDrequest:
+						LSBchecksumIn = byteIn;
+						process = LSBchecksumRecieved;
+						if(debugEnable == true){
+							Serial.println("LSB recieved");
+						}
+						break;
+					case readRequest:
+						argumentBuffer[argumentBufferAt] = byteIn;
+						argumentBufferAt++;
+						if(argumentBufferAt >= 2){
+							process = argumentsRecieved;
+						}
+						break;
+					case writeInstruction:
+						argumentBuffer[argumentBufferAt] = byteIn;
+						argumentBufferAt++;
+						if(argumentBufferAt >= 2 && argumentBufferAt >= argumentBuffer[1] + 2){
+							process = argumentsRecieved;
+						}
+						break;
+					case dataReturn:
+						argumentBuffer[argumentBufferAt] = byteIn;
+						argumentBufferAt++;
+						if(argumentBufferAt >= 1 && argumentBufferAt >= argumentBuffer[0] + 2){
+							process = argumentsRecieved;
+						}
+						break;
+				}
+				break;
+			case argumentsRecieved:
+				MSBchecksumIn = byteIn;
+				if(debugEnable == true){
+					Serial.println("LSB recieved");
+				}
+				process = MSBchecksumRecieved;
+				break;
+			case MSBchecksumRecieved:
+				LSBchecksumIn = byteIn;
+				if(debugEnable == true){
+					Serial.println("MSB recieved");
+				}
+				process = LSBchecksumRecieved;
+			case LSBchecksumRecieved:
+				if((MSBchecksumIn << 8) | LSBchecksumIn == checksumInbox){
+					process = executeInstruction;
+					if(debugEnable == true){
+						Serial.println("checksum ok. exucute instruction");
+					}
+				}
+				else{
+					process = idle;
+					instruction = transmissionErrorInstruction;
+					if(debugEnable == true){
+						Serial.print("checksum faulty. ");
+						Serial.print((MSBchecksumIn << 8) | LSBchecksumIn);
+						Serial.print("recieved. needed: ");
+						Serial.println(checksumInbox);
+					}
+					return -1;
+				}
+			case executeInstruction:
+			switch(instruction){
+				case IDrequest:
+					sendID();
+					process = idle;
+					break;
+				case readRequest:
+					readCommand();
+					process = idle;
+					break;
+				case writeInstruction:
+					writeCommand();
+					process = idle;
+					break;
+				/*
+				case IDreturn:
+					process = idle;
+					break;
+				case debugInfo:
+					process = idle;
+					break;
+				case debugStateCommand:
+					process = idle;
+					break;
+				case transmissionErrorInstruction:
+					process = idle;
+					break;
+					*/
+				default:
+					process = idle;
+  		}
+  		break;
+  		default:
+  		process = idle;
+  	}
+  }
+  if(millis() - timeoutTime > 150){
+  	process = idle;
+  	return -1;
+  }
+  return 0;
+}
+
+void FlowSerial::sendID(){
+	Serial.write(0xAA);
+	Serial.write(0x03);
+	Serial.write(0x01);
+	Serial.write(ID);
+	int serialSum = 0xAE + ID;
+	Serial.write(serialSum >> 8);
+	Serial.write(serialSum & 0x00FF);
+}
+
+void FlowSerial::readCommand(){
+	Serial.write(0xAA);
+	Serial.write(0x03);
+	Serial.write(argumentBuffer[1]);
+	int checksumOut = 0xAD + argumentBuffer[1];
+	for(int i = 0;i < argumentBuffer[1]; i++){
+		Serial.write(serialReg[i + argumentBuffer[0]]);
+		checksumOut += serialReg[i + argumentBuffer[0]];
+	}
+	Serial.write(char(checksumOut >> 8));
+	Serial.write(char(checksumOut));
+}
+
+void FlowSerial::writeCommand(){
+	for(int i = 0;i < argumentBuffer[1]; i++){
+		serialReg[i + argumentBuffer[0]] = argumentBuffer[i + 2];
+	}
+}
+
+void FlowSerial::recieveData(){
+	for(int i = 0; i < argumentBuffer[1]; i++){
+		inboxBuffer[i + inboxRegisterAt] = argumentBuffer[i + 2];
+		inboxAvailable++;
+		inboxRegisterAt++;
+	}
+}
+
+char FlowSerial::read(){
+	char charOut = inboxBuffer[inboxRegisterAt - inboxAvailable];
+	inboxAvailable--;
+	return charOut;
+}
+
+int FlowSerial::available(){
+	return inboxAvailable;
+}
+
+void FlowSerial::transmissionError(boolean resend){
+	Serial.write(0xAA);
+	Serial.write(0x06);
+	if(resend == true){
+		Serial.write(0x01);
+		Serial.write(0x00);
+		Serial.write(0xb1);
+	}
+	else{
+		Serial.write(0x00);
+		Serial.write(0x00);
+		Serial.write(0xb0);
+	}
+}
+
+void FlowSerial::sendDebugInfo(String text, int textLength){
+	Serial.write(0xAA);
+	Serial.write(0x04);
+	Serial.write(textLength);
+	int serialSum = 0xAE + textLength;
+	for(int i = 0; i < textLength; i++){
+		Serial.write(text[i]);
+		serialSum += text[i];
+	}
+	Serial.write(char(serialSum));
+	Serial.write(char(serialSum >> 8));
+}
+
+void FlowSerial::write(byte address, byte out[], int quantity){
+	Serial.write(0xAA);
+	Serial.write(0x02);
+	Serial.write(address);
+	Serial.write(quantity);
+	int serialSum = 0xAC + address + quantity;
+	for(int i = 0; i < quantity; i++){
+		Serial.write(out[i]);
+		serialSum += out[i];
+	}
+	Serial.write(char(serialSum));
+	Serial.write(char(serialSum >> 8));
+}
+
+void FlowSerial::write(byte address, byte out){
+	Serial.write(0xAA);
+	Serial.write(0x02);
+	Serial.write(address);
+	Serial.write(0x01);
+	int serialSum = 0xAD + address + out;
+	Serial.write(out);
+	Serial.write(char(serialSum));
+	Serial.write(char(serialSum >> 8));
+}
---
+++ flowSerialArduino.h
@@ -0,0 +1,47 @@
+/*
+flowSerial.h - Library for register based communication over rs-232 or usb
+Registers at both parties wich can be read or writed from.
+Is able to ferify the data with a checksum
+
+Copyright 2014
+Created by Jimmy van den Berg.
+June to August 2014.
+*/
+
+#ifndef flowSerial_h
+#define flowSerial_h
+
+#include "Arduino.h"
+
+class FlowSerial{
+public:
+	FlowSerial(int baudrate, int regSize, char IDinit);
+	char update();
+	char ID;
+	unsigned char serialReg[256];
+	char inboxBuffer[256];
+	boolean debugEnable;
+	char read();
+	int available();
+	void write(byte address, byte out[], int quantity);
+	void write(byte address, byte out);
+private:
+	unsigned char inboxRegisterAt;
+	unsigned char inboxAvailable;
+	unsigned char process;
+	unsigned char argumentBuffer[256];
+	unsigned char argumentBufferAt;
+	long timeoutTime;
+	int LSBchecksumIn;
+	int MSBchecksumIn;
+	int checksumInbox;
+	int instruction;
+	void sendID();
+	void readCommand();
+	void writeCommand();
+	void recieveData();
+	void transmissionError(boolean resend);
+	void sendDebugInfo(String text, int textLenght);
+};
+
+#endif
